f588ae1a4ada78ace0e80d2ba8a44cf77568638a
YoungJae Lee <66728514+realcare@users.noreply.github.com>
2023-11-13 21:48:55 +0900

    add : loading component

---
+++ src/components/service/loading.js
@@ -0,0 +1,20 @@
+import '../../css/components/service/loading.css';
+
+const Loading = () => {
+    return (
+        <div className="loadingContainer">
+            <div className="lds-roller">
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+        </div>
+    );
+};
+
+export default Loading;
