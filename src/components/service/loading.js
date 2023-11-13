daf2d4a5ee848ee3c303c19e36286aca62e90bff
YoungJae Lee <66728514+realcare@users.noreply.github.com>
2023-11-14 00:20:46 +0900

    fix : loading Box style and text

--- src/components/service/loading.js
+++ src/components/service/loading.js
@@ -1,19 +1,30 @@
 import '../../css/components/service/loading.css';
 
-const Loading = () => {
+const Loading = (props) => {
+    const {text} = props
     return (
+        <>
+        <div className="loadingBackground"/>
         <div className="loadingContainer">
-            <div className="lds-roller">
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
+            <div className="loadingBox">
+                <div className="lds-roller">
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                </div>
+                <div className="loadingText">{text}</div>
             </div>
         </div>
+        
+        </>
+      
+            
+       
     );
 };
 
